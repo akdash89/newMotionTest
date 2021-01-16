d9232479da24a6f9691781fce802055b1a4fef85
Amit <amitdash4717@gmail.com>
2021-01-16 22:11:44 +0530

    initial commit

---
+++ starwars-api/src/test/java/com/starwars/test/SWApiTest.java
@@ -0,0 +1,39 @@
+package com.starwars.test;
+
+import java.io.IOException;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.starwars.bo.People;
+import com.starwars.util.RetrieveUtil;
+
+public class SWApiTest {
+
+	private final String BASE_URL = "https://swapi.dev/api/";
+
+	@Test
+	public void testGetPeopleAPIById() throws ClientProtocolException, IOException {
+
+		HttpUriRequest request = new HttpGet(BASE_URL + "people/1/");
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+		People resource = RetrieveUtil.retrieveResourceFromResponse(response, People.class);
+		String contentType = null;
+		for (Header header : response.getAllHeaders()) {
+			if (header.getName().equals("Content-Type")) {
+				contentType = header.getValue();
+				break;
+			}
+		}
+		Assert.assertEquals("application/json", contentType);
+		Assert.assertNotNull(resource);
+		Assert.assertNotNull(resource.getName());
+	}
+
+}

3afd09e26df39ac583a4b77edd86e870047fc47d
akdash89 <amitdash4717@gmail.com>
2021-01-21 19:47:05 +0100

    Test scenario and report generation dependencies added

--- starwars-api/src/test/java/com/starwars/test/SWApiTest.java
+++ starwars-api/src/test/java/com/starwars/test/SWApiTest.java
@@ -11,6 +11,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.starwars.bo.Film;
 import com.starwars.bo.People;
 import com.starwars.util.RetrieveUtil;
 
@@ -34,6 +35,25 @@ public class SWApiTest {
 		Assert.assertEquals("application/json", contentType);
 		Assert.assertNotNull(resource);
 		Assert.assertNotNull(resource.getName());
+	}
+
+	@Test
+	public void testGetFilmById() throws ClientProtocolException, IOException {
+
+		HttpUriRequest request = new HttpGet(BASE_URL + "films/1/");
+		HttpResponse response = HttpClientBuilder.create().build().execute(request);
+		Film resource = RetrieveUtil.retrieveResourceFromResponse(response, Film.class);
+		String contentType = null;
+		for (Header header : response.getAllHeaders()) {
+			if (header.getName().equals("Content-Type")) {
+				contentType = header.getValue();
+				break;
+			}
+		}
+		Assert.assertEquals("application/json", contentType);
+		Assert.assertNotNull(resource);
+		Assert.assertEquals("A New Hope", resource.getTitle());
+
 	}
 
 }
